d43a5beb2fb819afb8c2dda368dd10a9a6cae38f
wc <1339835853@qq.com>
2022-04-03 13:34:24 +0800

    第五周-社区开发

--- src/main/java/com/nowcoder/community/entity/DiscussPost.java
+++ src/main/java/com/nowcoder/community/entity/DiscussPost.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.util.Date;
 
@@ -16,14 +20,35 @@ import java.util.Date;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+
+@Document(indexName = "discusspost", shards = 6, replicas = 3)
 public class DiscussPost {
+
+    @Id
     private int id;
-    private int userId;         //发布该帖子的用户Id
-    private String title;       //帖子的标题
-    private String content;     //帖子的内容
-    private int type;           //判断是否是置顶
-    private int status;         //判断是否是精华 2表示拉黑
-    private Date createTime;    //帖子的发布时间
-    private int commentCount;   //帖子的评论数量
-    private double score;       //帖子的分数
+
+    @Field(type = FieldType.Integer)
+    private int userId;
+
+    // 互联网校招
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
+    private String title;
+
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
+    private String content;
+
+    @Field(type = FieldType.Integer)
+    private int type;
+
+    @Field(type = FieldType.Integer)
+    private int status;
+
+    @Field(type = FieldType.Date)
+    private Date createTime;
+
+    @Field(type = FieldType.Integer)
+    private int commentCount;
+
+    @Field(type = FieldType.Double)
+    private double score;
 }
